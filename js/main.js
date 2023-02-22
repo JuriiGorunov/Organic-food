108cf0c2088750228d5e97c813658880991d993f
JuriiGorunov <pro100krash63rus@rambler.ru>
2023-02-22 20:48:18 +0400

    Добавлен Js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".customer-starts").starRating({
+    totalStars:5,
+    strokeColor: '#f1b908',
+    rateColor: '#f1b908',
+    starSize: 23,
+    readOnly: true,
+    strokeWidth: 20,
+  });
+});
