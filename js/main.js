d0f3c2d0d895e90587a7047fee784a15290ef19e
JuriiGorunov <pro100krash63rus@rambler.ru>
2023-02-23 15:17:17 +0400

    закончили стрелочки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".customer-starts").starRating({
-    totalStars:5,
+    totalStars: 5,
     strokeColor: '#f1b908',
     rateColor: '#f1b908',
     starSize: 23,
@@ -10,5 +10,17 @@ $(document).ready(function () {
 
   // slider
 
-  $(".customers-slider").slick();
+  $(".customers-slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="btn slider-btn slider-prev"></button>',
+    nextArrow: '<button class="btn slider-btn slider-next active"></button>',
+  });
+  // делаем переключение активных стрелочек
+
+  $('.slider-btn').click(function(){
+      $('.slider-btn').removeClass("active");
+      $(this).addClass("active")
+  })
 });
